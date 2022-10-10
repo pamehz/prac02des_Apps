4e540d0d85d4f3f4202de2354ba3d3801b015495
Pamela Zumbado <105823490+pamehz@users.noreply.github.com>
2022-10-10 17:29:51 -0600

    practica semana 3

---
+++ src/main/java/com/practica01desapps/Practica01desappsApplication.java
@@ -0,0 +1,13 @@
+package com.practica01desapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica01desappsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica01desappsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica01desapps/Practica01desappsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica01desapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica01desappsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
